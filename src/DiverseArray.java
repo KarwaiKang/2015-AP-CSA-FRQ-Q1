307b64176e3fd07285129fa3bc226fc0f4024d18
KarwaiKang <kkang9093@bths.edu>
2018-12-20 18:12:39 -0500

    Add test for rowSums method Add docs and comments Refactor index variable in rowSums method Change formatting in two-dimensional array declarations in Runner class

--- src/DiverseArray.java
+++ src/DiverseArray.java
@@ -1,4 +1,8 @@
 public class DiverseArray {
+    /**
+     * @param arr the one-dimensional array.
+     * @return the sum of the entries in the one-dimensional array {@code arr}.
+     */
     public static int arraySum(int[] arr) {
         int sum = 0;
         for (int i : arr)
@@ -6,13 +10,23 @@ public class DiverseArray {
         return sum;
     }
 
+    /**
+     * @param arr2D the two-dimensional array.
+     * @return a one-dimensional array in which the entry at index {@code k} is the sum of
+     * the entries of row {@code k} of the two-dimensional array {@code arr2D}.
+     */
     public static int[] rowSums(int[][] arr2D) {
         int[] sums = new int[arr2D.length];
-        for (int i = 0; i <arr2D.length; i++)
-            sums[i] = arraySum(arr2D[i]);
+        for (int k = 0; k < arr2D.length; k++)
+            sums[k] = arraySum(arr2D[k]);
         return sums;
     }
 
+    /**
+     * @param arr2D the two-dimensional array.
+     * @return {@code true}if all rows in {@code arr2D} have different row sums;
+     * {@code false} otherwise.
+     */
     public static boolean isDiverse(int[][] arr2D) {
         int[] sums = rowSums(arr2D);
         for (int i = 0; i < sums.length; i++) {
--- src/Runner.java
+++ src/Runner.java
@@ -1,20 +1,25 @@
 public class Runner {
     public static void main(String[] args) {
         int[][] mat1 = {
-                {1,3,2,7,3},
-                {10,10,4,6,2},
-                {5,3,5,9,6},
-                {7,6,4,2,1},
+                {1, 3, 2, 7, 3},
+                {10, 10, 4, 6, 2},
+                {5, 3, 5, 9, 6},
+                {7, 6, 4, 2, 1},
         };
 
         int[][] mat2 = {
-                {1,1,5,3,4},
-                {12,7,6,1,9},
-                {8,11,10,2,5},
-                {3,2,3,0,6},
+                {1, 1, 5, 3, 4},
+                {12, 7, 6, 1, 9},
+                {8, 11, 10, 2, 5},
+                {3, 2, 3, 0, 6},
         };
 
-        System.out.println(DiverseArray.isDiverse(mat1));
-        System.out.println(DiverseArray.isDiverse(mat2));
+        int[] sums = DiverseArray.rowSums(mat1);
+        StringBuilder sb = new StringBuilder("{");
+        for (int i : sums)
+            sb.append(i).append(", ");
+        System.out.println(sb.substring(0, sb.length() - 2) + "}"); // → {16, 32, 28, 20}
+        System.out.println(DiverseArray.isDiverse(mat1)); // → true
+        System.out.println(DiverseArray.isDiverse(mat2)); // → false
     }
 }
